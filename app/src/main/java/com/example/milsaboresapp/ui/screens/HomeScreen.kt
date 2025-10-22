417ee4cee9b684b8ccdca58f1b8bfe227a5c1c36
ivsantander-dot <iv.santander@duocuc.cl>
2025-10-21 21:40:32 -0300

    Terminada pantalla de inicio con articulos destacados

--- app/src/main/java/com/example/milsaboresapp/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/example/milsaboresapp/ui/screens/HomeScreen.kt
@@ -3,41 +3,70 @@ package com.example.milsaboresapp.ui.screens
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.milsaboresapp.R
+import com.example.milsaboresapp.data.remote.ProductosSource
+import com.example.milsaboresapp.ui.components.ProductCard
 import com.example.milsaboresapp.ui.theme.ColorLight
+import com.example.milsaboresapp.ui.theme.ColorPrimary
 
 @Composable
 fun HomeScreen(navigateToProductos: () -> Unit) {
+
+    val productosDestacados = ProductosSource.productos.filter { it.destacado }
+
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .background(ColorLight)
-            .padding(20.dp),
+            .background(ColorLight),
         verticalArrangement = Arrangement.spacedBy(20.dp)
     ) {
-        Text("¡Bienvenido a Mil Sabores!",
-            style = MaterialTheme.typography.headlineMedium,
-            modifier = Modifier.align(Alignment.CenterHorizontally)
-        )
-        Button(onClick = navigateToProductos) {
-            Text("Ver productos")
-        }
+
         Image(
-            painter = painterResource(id = R.drawable.logo),
+            painter = painterResource(id = R.drawable.cincuentaanios),
             contentDescription = "Logo App",
             modifier = Modifier
                 .fillMaxWidth()
-                .height(150.dp),
-            contentScale = ContentScale.Fit
+                .height(300.dp),
+            contentScale = ContentScale.FillWidth
         )
+
+        Text("¡Productos destacados!",
+            style = MaterialTheme.typography.headlineMedium,
+            fontSize = 25.sp,
+            modifier = Modifier
+                .align(Alignment.CenterHorizontally)
+        )
+        LazyRow {
+            items(productosDestacados) { producto ->
+                Box(
+                    modifier = Modifier
+                        .width(200.dp)
+                        .padding(5.dp)// define el ancho de cada tarjeta
+                ) {
+                    ProductCard(producto = producto)
+                }
+            }
+        }
+
+        Button(
+            onClick = navigateToProductos,
+            modifier = Modifier.align(Alignment.CenterHorizontally)
+
+        ) {
+            Text("Ver todos los productos!")
+        }
     }
 }
 
